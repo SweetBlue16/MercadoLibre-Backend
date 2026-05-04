1a9397c68c5736ff18c58c2bcb5face53df59cc4
Mauricio Noriega <mauricionoriega.sw@outlook.com>
2026-05-03 21:03:41 -0600

    chore: add prittier and entry point file

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server is running on port ${process.env.SERVER_PORT}`);
+});
