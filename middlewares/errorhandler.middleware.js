b5c2eb2f32bc991a381b61ee14711a537dc77e3f
Mauricio Noriega <mauricionoriega.sw@outlook.com>
2026-05-04 14:52:03 -0600

    feat: add images, error handling and bitacora middlewares

---
+++ middlewares/errorhandler.middleware.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const requestIp = require('request-ip');
+const ClaimTypes = require('../config/claimtypes');
+
+const errorHandler = (err, req, res, next) => {
+  let message =
+    'No se ha podido procesar la petición. Inténtelo nuevamente más tarde.';
+  const statusCode = err.statusCode || 500;
+  const ip = requestIp.getClientIp(req);
+
+  let email = 'Anónimo';
+  if (req.decodedToken) {
+    email = req.decodedToken[ClaimTypes.Name];
+  }
+
+  fs.appendFile(
+    'log/log.txt',
+    new Date() +
+      ` - ${statusCode} - ${ip} - ${email} - ${err.message || message}\n`,
+    (err) => {
+      if (err) {
+        console.error(err);
+      }
+    }
+  );
+
+  if (process.env.NODE_ENV === 'development') {
+    message = err.message || message;
+    res.status(statusCode).json({
+      status: statusCode,
+      message: message,
+      stack: err.stack,
+    });
+  } else {
+    res.status(statusCode).send({ message: message });
+  }
+};
+
+module.exports = errorHandler;
